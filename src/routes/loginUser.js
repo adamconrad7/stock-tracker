6275296c50753dd4935630f68ae0d3535ade7b40
okazamax <okazamax@oregonstate.edu>
2019-12-11 19:32:22 -0800

    fixed user auth

--- src/routes/loginUser.js
+++ src/routes/loginUser.js
@@ -2,19 +2,20 @@ const express = require('express');
 const { createViewContext } = require('../utils');
 const Cryptr = require('cryptr');
 cryptr = new Cryptr('secretKey');
+const bcrypt = require('bcryptjs');
 
-// Nodejs encryption with CTR
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-var key = crypto.randomBytes(32);
-var iv = crypto.randomBytes(16);
+//// Nodejs encryption with CTR
+//const crypto = require('crypto');
+//const algorithm = 'aes-256-cbc';
+//var key = crypto.randomBytes(32);
+//var iv = crypto.randomBytes(16);
 
-function encrypt(text) {
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return encrypted.toString('hex')
-}
+//function encrypt(text) {
+//    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+//    let encrypted = cipher.update(text);
+//    encrypted = Buffer.concat([encrypted, cipher.final()]);
+//    return encrypted.toString('hex')
+//}
 
 const router = express.Router();
 var connection = require('./../config');
@@ -41,32 +42,42 @@ router.post('/user/login', (req, res, next) => {
             })
         } else {
             //var localPassword = cryptr.encrypt(req.body.uPass);
-            key = results[0].key1;
-            iv = results[0].iv;
-            var localPassword = encrypt(req.body.uPass);
-            console.log("db key:");
-            console.log(results[0].iv);
-            console.log("db iv:");
-            console.log(results[0].key1);
-            console.log("db password:");
-            console.log(results[0].Password);
-            console.log("input password:");
-            console.log(localPassword);
+            //key = results[0].key1;
+            //iv = results[0].iv;
+            //var localPassword = encrypt(req.body.uPass);
+
             if (results.length) {
-                if (localPassword == results[0].Password) {
-                    res.json({
-                        status: true,
-                        message: 'Successfully authenticated'
-                    })
-                    context.message = 'Successfully authenticated.';
-                    res.render('watchlist', context);
-                } else {
-                    res.json({
-                        status: false,
-                        message: "Username and password do not match"
-                    });
-                    context.message = 'Username and password do not match.';
-                }
+
+                bcrypt.compare(req.body.uPass, results[0].Password, function (err, isMatch) {
+                    if (err) {
+                        throw err
+                    } else if (!isMatch) {
+                        console.log("Password doesn't match!")
+                        context.message = "Password doesn't match.";
+                        res.render('user-login', context);
+                    } else {
+                        console.log("Password matches!")
+                        context.message = "Passwords match, logged in.";
+                        res.render('user-login', context);
+                    }
+                })
+
+
+
+                //if (localPassword == results[0].Password) {
+                //    res.json({
+                //        status: true,
+                //        message: 'Successfully authenticated'
+                //    })
+                //    context.message = 'Successfully authenticated.';
+                //    res.render('watchlist', context);
+                //} else {
+                //    res.json({
+                //        status: false,
+                //        message: "Username and password do not match"
+                //    });
+                //    context.message = 'Username and password do not match.';
+                //}
             }
             else {
                 res.json({
--- src/routes/registerUser.js
+++ src/routes/registerUser.js
@@ -2,22 +2,66 @@ const express = require('express');
 const { createViewContext } = require('../utils');
 const Cryptr = require('cryptr');
 cryptr = new Cryptr('secretKey');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
-// Nodejs encryption with CTR
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-const key1 = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
+//bcrypt.genSalt(saltRounds, function (err, salt) {
+//    if (err) {
+//        throw err
+//    } else {
+//        bcrypt.hash(password, salt, function (err, hash) {
+//            if (err) {
+//                throw err
+//            } else {
+//                console.log("nonese:")
+//                console.log(hash)
+//            }
+//        })
+//    }
+//})
 
-console.log(key1);
-console.log(iv);
 
-function encrypt(text) {
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key1), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return encrypted.toString('hex')
-};
+//bcrypt.genSalt(saltRounds, function (err, salt) {
+//    if (err) {
+//        throw err
+//    } else {
+//        bcrypt.hash(password, salt, function (err, hash) {
+//            if (err) {
+//                throw err
+//            } else {
+//                console.log("nonese2:")
+//                console.log(hash)
+//                bcrypt.compare("mypass1233", hash, function (err, isMatch) {
+//                    if (err) {
+//                        throw err
+//                    } else if (!isMatch) {
+//                        console.log("Password doesn't match!")
+//                    } else {
+//                        console.log("Password matches!")
+//                    }
+//                })
+//            }
+//        })
+//    }
+//})
+
+
+
+//Nodejs encryption with CTR
+//const crypto = require('crypto');
+//const algorithm = 'aes-256-cbc';
+//const key1 = crypto.randomBytes(32);
+//const iv = crypto.randomBytes(16);
+
+//console.log(key1);
+//console.log(iv);
+
+//function encrypt(text) {
+//    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key1), iv);
+//    let encrypted = cipher.update(text);
+//    encrypted = Buffer.concat([encrypted, cipher.final()]);
+//    return encrypted.toString('hex')
+//};
 
 const router = express.Router();
 
@@ -52,19 +96,35 @@ router.post('/user/add', (req, res, next) => {
             console.log(req.body.uName, req.body.uPass);
             //var encryptedString = cryptr.encrypt(req.body.uPass);
 
-            var encryptedString = encrypt(req.body.uPass);
-            console.log(key1);
-            console.log(iv);
-            req.db.query(
-                'INSERT INTO User (UserID, Password, lName, fName, key1, iv) VALUES (?,?,?,?,?,?)',
-                [req.body.uName, encryptedString, req.body.lName, req.body.fName, key1, iv],
-                err => {
-                    if (err) return next(err);
-                    console.log("No error");
-                    context.message = 'Record added successfully, please login.';
-                    res.render('user-add', context);
+            //bcrypt.hash(req.body.uPass, 10, function (err, hash) {
+
+            //}
+            //var encryptedString = encrypt(req.body.uPass);
+
+            bcrypt.genSalt(saltRounds, function (err, salt) {
+                if (err) {
+                    throw err
+                } else {
+                    bcrypt.hash(req.body.uPass, salt, function (err, hash) {
+                        if (err) {
+                            throw err
+                        } else {
+                            req.db.query(
+                                'INSERT INTO User (UserID, Password, lName, fName) VALUES (?,?,?,?)',
+                                [req.body.uName, hash, req.body.lName, req.body.fName],
+                                err => {
+                                    if (err) return next(err);
+                                    console.log("No error");
+                                    context.message = 'Record added successfully, please login.';
+                                    res.render('user-add', context);
+                                }
+                            );
+                        }
+                    })
                 }
-            );
+            })
+
+            
 
         }
     });
